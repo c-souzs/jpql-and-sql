819551008afa1460ef18564706259dcf951173bf
Caio Souza <souzsdev@gmail.com>
2024-12-26 11:37:36 -0300

    first commit

---
+++ src/main/java/com/souzs/jpql_and_sql/JpqlAndSqlApplication.java
@@ -0,0 +1,13 @@
+package com.souzs.jpql_and_sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpqlAndSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpqlAndSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/souzs/jpql_and_sql/JpqlAndSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.souzs.jpql_and_sql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpqlAndSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
